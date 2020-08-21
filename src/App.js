0dfa3bb066d4f3fd8eb611c4cf4edd9470976182
refuse-c <xiangbh@outlook.com>
2020-08-21 11:49:26 +0800

    create header file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@
  * @Author: REFUSE_C
  * @Date: 2020-08-18 17:57:51
  * @LastEditors: refuse_c
- * @LastEditTime: 2020-08-18 19:24:15
+ * @LastEditTime: 2020-08-21 11:45:32
  * @Description: 
  */
 import React from 'react';
+import Header from './common/components/header/Headers';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Emusic
     </div>
   );
