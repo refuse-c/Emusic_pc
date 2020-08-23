a7faa9cc2e27735b75646c3dfd5dded062a3c2ed
refuse-c <@outlook.com>
2020-08-24 07:57:17 +0800

    routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,52 @@
  * @Author: REFUSE_C
  * @Date: 2020-08-18 17:57:51
  * @LastEditors: refuse_c
- * @LastEditTime: 2020-08-23 13:48:18
+ * @LastEditTime: 2020-08-24 07:56:47
  * @Description: 
  */
 import React from 'react';
 import Header from '@components/header/Header';
 import Footer from '@components/footer/Footer';
 import './App.scss';
-
+import routes from '@/router/router';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import debounce from '@/common/utils/debounce';// 防抖
+global.debounce = debounce;
 function App() {
   return (
     <div className="App">
       <Header />
-      Emusic
+      <Router>
+        {routes.map((route, key) => {
+          if (route.exact) {
+            return (
+              <Route
+                key={key}
+                exact
+                path={route.path}
+                render={(props) => (
+                  <route.component {...props} routes={route.routes} />
+                )}
+              />
+            );
+          } else {
+            return (
+              <Route
+                key={key}
+                path={route.path}
+                render={(props) => (
+                  <route.component {...props} routes={route.routes} />
+                )}
+              />
+            );
+          }
+        })}
+        <Redirect to="/home/find" />
+        {/* <Redirect to="/home/singerdetail30285885/" /> */}
+        {/* <Redirect to="/home/find/rankingList" /> */}
+        {/* <Redirect to="/home/albumdetail4546" /> */}
+        {/* <Redirect to="/player" /> */}
+      </Router>
       <Footer />
     </div>
   );
