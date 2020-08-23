d164f4cca229e062d7e4e49cfccef03557f815d8
refuse-c <@outlook.com>
2020-08-23 13:51:11 +0800

    add code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@
  * @Author: REFUSE_C
  * @Date: 2020-08-18 17:57:51
  * @LastEditors: refuse_c
-<<<<<<< HEAD
- * @LastEditTime: 2020-08-19 17:38:52
-=======
- * @LastEditTime: 2020-08-21 13:31:33
->>>>>>> c0b8dedc6a2449d30868e69c1b07ecb033cb9a6b
+ * @LastEditTime: 2020-08-23 13:48:18
  * @Description: 
  */
 import React from 'react';
