447435a0ccf03c07cfa48e4072ff2a9a188d4c1f
refuse-c <@outlook.com>
2020-09-01 23:34:32 +0800

    form

--- src/common/components/modal/LoginModal.js
+++ src/common/components/modal/LoginModal.js
@@ -2,61 +2,58 @@
  * @Author: REFUSE_C
  * @Date: 2020-08-28 21:48:58
  * @LastEditors: REFUSE_C
- * @LastEditTime: 2020-09-01 18:05:38
+ * @LastEditTime: 2020-09-01 23:32:31
  * @Description 登录弹窗
  */
 import React, { Component } from 'react'
 import { Modal, Form, Input } from 'antd'
-
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { modalPower } from '@/store/actions';
 import { IS_SHOW_LOGIN } from '@/store/actionTypes';
-
-let T1;
-const FormItem = Form.Item
-
+const FormItem = Form.Item;
 class LoginModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
+      phone: '13272946536',
+      password: '123456'
     }
   }
 
-
-  handleCancel = () => {
-    clearTimeout(T1)
-    this.props.handelModalPower({ type: IS_SHOW_LOGIN, data: false });
-    this.setState({ loading: false });
-
-  };
 
   callback = key => {
     console.log(key);
   }
 
-  handleOk = async () => {
-    const values = await this.props.form.validateFields();
-    //values
-    console.log("values", values);
+  onFinish = e => {
+    this.formRef.current.validateFields()
+      .then(values => {
+        this.formRef.current.resetFields();
+        console.dir(values);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // 关闭登录弹窗
+    this.props.handelModalPower({ type: IS_SHOW_LOGIN, data: false });
+  }
 
-    clearTimeout(T1)
-    this.setState({ loading: true });
-    T1 = setTimeout(() => {
-      this.setState({ loading: false });
-      this.props.handelModalPower({ type: IS_SHOW_LOGIN, data: false });
-    }, 3000);
-  };
+  onCancel = e => {
+    this.formRef.current.resetFields();
+    // 关闭登录弹窗
+    this.props.handelModalPower({ type: IS_SHOW_LOGIN, data: false });
+    this.setState({ loading: false });
+  }
 
-
+  formRef = React.createRef();
   render() {
     const { showModal } = this.props;
     const { loading } = this.state;
-    const { getFieldDecorator } = this.props.form;
     return (
       <div>
+
         <Modal
           visible={showModal}
           title="小主,请选择登录的方式"
@@ -65,30 +62,50 @@ class LoginModal extends Component {
           width={400}
           maskClosable={false}
           confirmLoading={loading}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
+          onOk={this.onFinish}
+          onCancel={this.onCancel}
         >
-          <Form>
-            <FormItem label="账号">
-              {getFieldDecorator('phone', {
-                initialValue: '13272946536',
-                rules: [{ required: true, message: '请输入手机号码' }],
-              })(
-                <Input placeholder="请输入手机号码" />
-              )}
-            </FormItem>
-            <FormItem label="密码">
-              {getFieldDecorator('password', {
-                initialValue: '123456',
-                rules: [{
-                  required: true, message: '请输入登录密码'
-                }],
-              })(
-                <Input placeholder="请输入登录密码" />
-              )}
-            </FormItem>
-          </Form>
-        </Modal >
+          < Form ref={this.formRef} name="loginForm" preserve={false} autoComplete="off">
+            <FormItem
+              label="账号"
+              name="phone"
+              initialValue=""
+              extra="*目前仅支持使用手机号码登录"
+              getValueFromEvent={(e) => e.target.value.replace(/\D/g, '')}
+              rules={
+                [
+                  {
+                    required: true,
+                    message: "账号不能为空"
+                  },
+                  {
+                    pattern: /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$/,
+                    message: "您输入的手机号码有误"
+                  },
+                ]}
+            >
+              <Input type="tel" maxLength={11} placeholder="请输入手机号码" />
+
+            </FormItem >
+            <FormItem label="密码"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: '密码不能为空'
+                },
+                {
+                  pattern: /^\S*(?=\S{6,})\S*$/,
+                  message: "密码最小6位"
+                },
+              ]
+              }
+            >
+              <Input type="password" maxLength={20} placeholder="请输入登录密码" />
+
+            </FormItem >
+          </Form >
+        </Modal>
       </div >
     );
   }
@@ -97,7 +114,7 @@ class LoginModal extends Component {
 
 
 
-const mapStateToprops = state => {
+const mapStateToProps = state => {
   console.log(state)
   return {
     modalPower: state.modalPower,
@@ -109,6 +126,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-// constaaa Refuse = ;
-
-export default connect(mapStateToprops, mapDispatchToProps)(Form.create()(LoginModal));
+export default connect(mapStateToProps, mapDispatchToProps)(LoginModal)
