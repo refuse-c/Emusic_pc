0883b0121db5dc7a3c23a37ab9403aafd8f39e35
refuse-c <xiangbh@outlook.com>
2020-12-16 19:08:56 +0800

    add code

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Emusic_pc 2.0
+<!--
+ * @Author: REFUSE_C
+ * @Date: 2020-11-19 09:21:07
+ * @LastEditors: REFUSE_C
+ * @LastEditTime: 2020-12-16 17:55:07
+ * @Description: git remote set-url --add origin https://gitee.com/refuse-c/Emusic_pc.git
+-->
 
+# Emusic_pc 2.0
 
 [技术交流群](https://jq.qq.com/?_wv=1027&k=9nTPn8B7)
 
+## 音乐 api 来源
 
-## 音乐api来源
 [Binaryify/NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi)
 
+## gitee address
 
+[Emusic_pc](https://gitee.com/refuse-c/Emusic_pc.git)
--- src/common/api/video.js
+++ src/common/api/video.js
@@ -2,7 +2,7 @@
  * @Author: REFUSE_C
  * @Date: 2020-12-08 14:30:12
  * @LastEditors: REFUSE_C
- * @LastEditTime: 2020-12-15 23:14:19
+ * @LastEditTime: 2020-12-16 19:03:59
  * @Description:
  */
 import { getRequest } from './request';
@@ -53,15 +53,3 @@ export const videoDetail = params => {
 export const videoUrl = params => {
   return getRequest('/video/url', params)
 }
-
-// alg: "rt"
-// artistId: 1030001
-// artistName: "周深"
-// artists: (2)[{ … }, { … }]
-// briefDesc: null
-// cover: "http://p4.music.126.net/hjX9mnaNGLG2VDdwrvl2JA==/109951165437169605.jpg"
-// desc: null
-// duration: 35000
-// id: 10966915
-// mark: 0
-// name: "《不完美人生指南》周深&王子篇"
